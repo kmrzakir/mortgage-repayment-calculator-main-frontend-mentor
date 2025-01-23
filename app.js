f935f2af19ac5ed1dd29bd2aa5d51679a5c6d516
zakir <zakirmustafa444@gmail.com>
2025-01-23 15:57:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const calculateBtn = document.getElementById("calculate_btn");
+
+
+// const mortageAmountContainer =
+//   document.getElementsByClassName("mortage_amount")[0];
+
+// calculateBtn.addEventListener("click", () => {
+//   alert("pp");
+//   checkIsAllFieldsFilled();
+// });
+
+// function checkIsAllFieldsFilled() {
+//   // Select the input element inside the .mortage_and_rate section
+//   const inputElement = mortageAndRate.querySelector('input[type="number"]')[0];
+
+//   // Select the first <p> inside the same section (years)
+//   let yearsParagraph = mortageAndRate.querySelector("div > p");
+
+//   // Select the <p> with the class "error"
+//   let errorParagraph = mortageAndRate.querySelector(".error");
+//   if (inputElement[0].value.trim() == "") {
+//     errorParagraph[0].style.display = "block";
+//     errorParagraph[0].style.color = "red";
+//   }
+  
+// }
